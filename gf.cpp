3b40909def9b8259981f642b061ac8dbbf250041
daniel.guterding <daniel.guterding@gmail.com>
2015-08-19 14:17:49 +0200

    Fixed getting interpolated GF values so that for tau=beta point is picked from first period.

--- gf.cpp
+++ gf.cpp
@@ -22,10 +22,13 @@ fptype ImaginaryTimeGreensFunction::get_interpolated_value(const fptype tau){
   //obtains values between the actually stored values of the gf by interpolating stored values linearly
   //antiperiodicity of the GF in imaginary time is automatically taken care of
   int p = floor(tau/beta); //counts the number of periods we have to translate back 
-  fptype tauprime = tau - p*beta; //this is the timepoint in the original interval where we want to look up value 
-  int lidx = floor(tauprime/dtau); //this is the lower index of the stored timepoints neighbouring the timepoint of interest
-  fptype taurem = tauprime - lidx*dtau; //this is the remainder of tauprime within the interval of interest
-  fptype gfval = gf[lidx] + (gf[lidx+1] - gf[lidx])/dtau*taurem;
-  fptype sign = pow(-1,p); //get the sign corresponding to fermionic antiperiodicity
+  if(fabs(tau - beta) < 1e-13){ //fix last point which is tau=beta to be picked from first perio regardless
+    p = 0;
+  }
+  const fptype tauprime = tau - p*beta; //this is the timepoint in the original interval where we want to look up value 
+  const int lidx = floor(tauprime/dtau); //this is the lower index of the stored timepoints neighbouring the timepoint of interest
+  const fptype taurem = tauprime - lidx*dtau; //this is the remainder of tauprime within the interval of interest
+  const fptype gfval = gf[lidx] + (gf[lidx+1] - gf[lidx])/dtau*taurem;
+  const fptype sign = pow(-1,p); //get the sign corresponding to fermionic antiperiodicity
   return sign*gfval;
 }
