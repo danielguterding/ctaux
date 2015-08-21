f65e00912c1f6357a3f5a4ded10426bea6b2da0d
daniel.guterding <daniel.guterding@gmail.com>
2015-08-21 09:59:33 +0200

    Implemented case switch for the various RNGs interfaces.

--- solver.cpp
+++ solver.cpp
@@ -76,9 +76,24 @@ void CTAUXSolver::initialize(){
   
   const int seed = noderank; //use noderank as seed
   config_ptr = new CTAUXConfiguration;
-  //rng_ptr = new RNG_StdMersenne(seed);
-  rng_ptr = new RNG_Philox4x32(seed);
   
+  //choose random number generator
+  const int rngidx = 1;
+  switch(rngidx){
+    case 0:
+      rng_ptr = new RNG_StdMersenne(seed);
+      break;
+    case 1:
+      rng_ptr = new RNG_Threefry4x64(seed);
+      break;
+    case 2:
+      rng_ptr = new RNG_Philox4x64(seed);
+      break;
+    case 3:
+      rng_ptr = new RNG_Philox4x32(seed);
+      break;
+  }
+      
   const fptype tau = p.beta*rng_ptr->get_value();
   const bool spin = floor(2*rng_ptr->get_value());
   config_ptr->insert(tau, spin);
